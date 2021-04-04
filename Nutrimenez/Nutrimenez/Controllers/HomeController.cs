2f6d82347eddd34a18dc8d3e9688c6ea4dce6089
CGMDAW <gonzalez20606@iesmarenostrum.com>
2021-04-04 08:58:15 +0200

    actualizacion proyecto

--- Nutrimenez/Nutrimenez/Controllers/HomeController.cs
+++ Nutrimenez/Nutrimenez/Controllers/HomeController.cs
@@ -40,6 +40,21 @@ namespace Nutrimenez.Controllers
             return View();
         }
 
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
